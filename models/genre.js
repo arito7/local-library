59fd6c9652489dad5021c9e423e5e2ef3cd9916e
Yuji T <accmanager1990@gmail.com>
2022-03-19 20:12:25 +0900

    Implement Genre List

--- models/genre.js
+++ models/genre.js
@@ -6,7 +6,7 @@ const GenreSchema = new Schema({
   name: { type: String, required: true, minlength: 3, maxlength: 100 },
 });
 
-GenreSchema.virtual('url').get(() => {
+GenreSchema.virtual('url').get(function () {
   return `/catalog/genre/${this._id}`;
 });
 
